7bd7dd606c0e1a2475400e74ab42e18778cbb96b
Max Shepard <widget@msgor.com>
2017-08-28 18:02:15 -0400

    Added fail2ban

--- provision.sh
+++ provision.sh
@@ -55,7 +55,7 @@ apt-get install -y --no-install-recommends curl python-software-properties pytho
     curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash - && \
     apt-get remove -y nodejs npm 'vim.*' && \
     apt-get update && \
-    apt-get install -y linode-cli w3m unattended-upgrades gdebi-core virtualenv python-virtualenv python-pip checkinstall dos2unix cadaver nmap graphviz expect supervisor vim-nox-py2 tmux htop mysql-client postgresql-client git nodejs docker-engine linux-image-extra-virtual linux-image-extra-$(uname -r) apt-transport-https ca-certificates pwgen build-essential ssl-cert ipcalc libpam-duo acl jq imagemagick sqlite3 || exit 1
+    apt-get install -y fail2ban linode-cli w3m unattended-upgrades gdebi-core virtualenv python-virtualenv python-pip checkinstall dos2unix cadaver nmap graphviz expect supervisor vim-nox-py2 tmux htop mysql-client postgresql-client git nodejs docker-engine linux-image-extra-virtual linux-image-extra-$(uname -r) apt-transport-https ca-certificates pwgen build-essential ssl-cert ipcalc libpam-duo acl jq imagemagick sqlite3 || exit 1
 
 #Systempony
 gdebi -n "$CURDIR/systempony.deb"
